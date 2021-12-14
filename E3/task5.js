2df8e743350ead114edf6dd7746e76be573c4f0d
skripkalisa <skripkalisa.msk@ya.ru>
2021-12-14 06:38:31 +0300

    mod E3 is almost ready

--- E3/task5.js
+++ E3/task5.js
@@ -6,3 +6,22 @@
 
 Протестируйте функцию на любых значениях и выведите результат в консоль.
  */
+
+const power = (x, n) => {
+  if (x < 0 || n < 0) return false
+  let result = 1
+  for (let i = 0; i < n; i++) {
+    result *= x
+  }
+  return result
+}
+
+console.log(power(2, 4))
+console.log(power(3, 4))
+console.log(power(5, 3))
+console.log(power(10, 6))
+console.log(power(64, 2))
+console.log(power(-64, 2))
+console.log(power(64, -2))
+console.log(power(0, 2))
+console.log(power(2, 0))
